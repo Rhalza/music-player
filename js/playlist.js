18fff063d735f864e7dfec37c21bb698dc3e4090
Rhalza <ryx.tech.official@gmail.com>
2025-10-04 18:12:35 -0400

    Update playlist.js for no spaces

--- js/playlist.js
+++ js/playlist.js
@@ -5,17 +5,14 @@ class Playlist {
         this.onSongSelect = onSongSelect;
         this.playlistEl = document.getElementById('playlist');
         this.draggedItem = null;
-
         document.getElementById('upload-btn').addEventListener('click', () => document.getElementById('song-upload').click());
         document.getElementById('upload-folder-btn').addEventListener('click', () => document.getElementById('folder-upload').click());
         document.getElementById('song-upload').addEventListener('change', (e) => this.handleFiles(e.target.files));
         document.getElementById('folder-upload').addEventListener('change', (e) => this.handleFiles(e.target.files));
-        
         this.playlistEl.addEventListener('dragstart', (e) => { this.draggedItem = e.target.closest('li'); });
         this.playlistEl.addEventListener('dragover', (e) => { e.preventDefault(); const target = e.target.closest('li'); if(target && target !== this.draggedItem) target.parentNode.insertBefore(this.draggedItem, target); });
         this.playlistEl.addEventListener('drop', () => { this.updatePlaylistOrder(); this.draggedItem = null; });
     }
-
     async handleFiles(files) {
         const audioFiles = Array.from(files).filter(file => file.type.startsWith('audio/'));
         for (const file of audioFiles) {
@@ -27,7 +24,6 @@ class Playlist {
         }
         this.render();
     }
-
     readTags(file) {
         return new Promise(resolve => {
             window.jsmediatags.read(file, {
@@ -36,14 +32,12 @@ class Playlist {
             });
         });
     }
-    
     selectSong(index) {
         if (index < 0 || index >= this.playlist.length) return;
         this.currentSongIndex = index;
         this.onSongSelect(this.playlist[this.currentSongIndex]);
         this.render();
     }
-
     render() {
         this.playlistEl.innerHTML = '';
         this.playlist.forEach((song, index) => {
@@ -52,7 +46,6 @@ class Playlist {
             li.dataset.index = index;
             li.draggable = true;
             li.innerHTML = `<i class="fas fa-grip-vertical drag-handle"></i><span class="song-title-playlist">${song.title || song.file.name}</span><i class="fas fa-times remove-song-btn"></i>`;
-            
             li.addEventListener('click', (e) => {
                 if (e.target.classList.contains('remove-song-btn')) {
                     this.removeSong(index);
@@ -63,7 +56,6 @@ class Playlist {
             this.playlistEl.appendChild(li);
         });
     }
-    
     removeSong(index) {
         this.playlist.splice(index, 1);
         if(this.currentSongIndex >= index) {
@@ -71,7 +63,6 @@ class Playlist {
         }
         this.render();
     }
-    
     updatePlaylistOrder() {
         const newPlaylist = [];
         this.playlistEl.querySelectorAll('li').forEach(li => {
@@ -81,7 +72,6 @@ class Playlist {
         this.currentSongIndex = this.playlist.findIndex(song => song.file.name === this.playlist[this.currentSongIndex].file.name);
         this.render();
     }
-    
     next() { this.selectSong((this.currentSongIndex + 1) % this.playlist.length); }
     prev() { this.selectSong((this.currentSongIndex - 1 + this.playlist.length) % this.playlist.length); }
 }

24dd50e6b2a1fa081d3be22172369c588b19b3d0
Rhalza <ryx.tech.official@gmail.com>
2025-10-04 18:13:02 -0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -13,24 +13,20 @@ const UI = {
             document.getElementById('center-image').src = 'rhalza.png';
         }
     },
-
     updateProgressBar(audio) {
         const { duration, currentTime } = audio;
         if (duration) {
             const progressPercent = (currentTime / duration) * 100;
             document.querySelector('.progress-bar').style.width = `${progressPercent}%`;
-            
             const formatTime = (time) => {
                 const minutes = Math.floor(time / 60);
                 const seconds = Math.floor(time % 60).toString().padStart(2, '0');
                 return `${minutes}:${seconds}`;
             };
-            
             document.querySelector('.current-time').textContent = formatTime(currentTime);
             document.querySelector('.duration').textContent = formatTime(duration);
         }
     },
-    
     setPlayPause(playing) {
         document.getElementById('play-pause-btn').classList.toggle('playing', playing);
     }
